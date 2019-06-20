8ebf0c4ac24b73251c2280221f645a261faeca0e
Wilder Schaaf <wilderschaaf@gmail.com>
2019-06-19 19:41:07 -0700

    working on the grid styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Square extends React.Component {
+  render() {
+    return (
+      <div className="square">
+      </div>
+    );
+  }
+}
+
+
+class Grid extends React.Component {
+  render() {
+    return (
+      <div className = "game-container">
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+        </div>
+      </div>
+    );
+  }
+  renderSquare(i){
+    return <Square value={i}/>
+  }
+}
+
+class Deck extends React.Component {
+
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Grid />
+        </div>
+        <div className="deck">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
